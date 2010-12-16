6195081aceedba6a7e7d22df61aee2941fec51f4
Syrus Akbary <dimension.net@gmail.com>
2010-12-16 04:15:04 +0100

    Version de visualización final

--- urls.py
+++ urls.py
@@ -18,3 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/absolutepath'}),
+    )
